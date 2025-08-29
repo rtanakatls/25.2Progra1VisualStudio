f7f20f95a66128236ee3fcc8636cb3b7029131ae
Ricardo Tanaka Terukina <rtanaka92@gmail.com>
2025-08-28 21:53:08 -0500

    Created project

---
+++ Character.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Progra1252
+{
+    internal class Character
+    {
+        private string name;
+        private int health;
+
+        public string Name { get { return name; } }
+        public int Health { get { return health; } }
+
+        public Character(string name, int health)
+        {
+            this.name = name;
+            this.health = health;
+        }
+
+    }
+}
---
+++ Menu.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Progra1252
+{
+    internal class Menu
+    {
+        private Character player;
+        public void Execute()
+        {
+            Console.WriteLine("Se inició el programa");
+            CreatePlayer();
+        }
+
+        private void CreatePlayer()
+        {
+            string name;
+            int health;
+            Console.WriteLine("Introduce el nombre del jugador:");
+            name = Console.ReadLine();
+            Console.WriteLine("Introduce la vida del jugador:");
+            health = int.Parse(Console.ReadLine());
+            player = new Character(name, health);
+        }
+    }
+}
